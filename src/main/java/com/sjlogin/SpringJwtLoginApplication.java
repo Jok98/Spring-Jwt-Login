d8d50ed128a5976045a2d5d7066310821d1f0b55
Jok98 <matteo.moi1998@gmail.com>
2024-06-16 21:20:12 +0200

    Init

---
+++ src/main/java/com/sjlogin/SpringJwtLoginApplication.java
@@ -0,0 +1,13 @@
+package com.sjlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sjlogin/SpringJwtLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
